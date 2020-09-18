0253206cb3190ad4ebd2e276d4f0b27f814d04e1
Mike Bronner <mike@genealabs.com>
2020-09-17 19:04:11 -0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,8 @@
 
 use GeneaLabs\LaravelNovaBlog\Http\Controllers\AuthorsController;
 use GeneaLabs\LaravelNovaBlog\Http\Controllers\BlogsController;
-use GeneaLabs\LaravelNovaBlog\Http\Controllers\CategoriesController;
 use GeneaLabs\LaravelNovaBlog\Http\Controllers\PostsController;
 
 Route::resource("authors", AuthorsController::class);
 Route::resource("blogs", BlogsController::class);
-Route::resource("categories", CategoriesController::class);
 Route::resource("posts", PostsController::class);
